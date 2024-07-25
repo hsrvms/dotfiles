f2a292a4fbbf5d5a0670fdd6908e43b41f7faec7
hsrvms <59848940+hsrvms@users.noreply.github.com>
2024-07-25 18:51:00 +0300

    add lazyvim config

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume" }
+)
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Explorer" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "One line up the selection." })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "One line down the selection." })
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
