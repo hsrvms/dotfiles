99a6a706bc99377ff3092c7c4c1c15aaf3c9b1f8
hsrvms <59848940+hsrvms@users.noreply.github.com>
2026-05-21 13:21:40 +0300

    keyboard layout

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #     fortune
 # end
 
-abbr -a fzf "fzf --ansi"
+abbr -a fzfa "fzf --ansi"
 abbr -a hxf 'helix $(fzf --ansi -m --preview="bat --color=always {}")'
 abbr -a cs "gh cs"
 abbr -a dc devcontainer
@@ -15,3 +15,7 @@ alias hx helix
 # alias zed zeditor
 
 alias qutebrowser="env QT_QPA_PLATFORM=xcb qutebrowser"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
