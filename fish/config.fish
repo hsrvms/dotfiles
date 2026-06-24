e71cb20499f09b842dd73f6fce83f8afd2c00971
hsrvms <59848940+hsrvms@users.noreply.github.com>
2026-06-24 16:47:47 +0300

    add zellij config

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,12 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-# overwrite greeting
-# potentially disabling fastfetch
-# function fish_greeting
-#     fortune
-# end
+# set -Ux EDITOR (which micro)
 
 abbr -a fzfa "fzf --ansi"
-abbr -a hxf 'helix $(fzf --ansi -m --preview="bat --color=always {}")'
 abbr -a cs "gh cs"
 abbr -a dc devcontainer
+abbr -a gotest 'GOTEST_SKIPNOTESTS="true" gotest'
 
-alias hx helix
 # alias zed zeditor
-
-alias qutebrowser="env QT_QPA_PLATFORM=xcb qutebrowser"
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
